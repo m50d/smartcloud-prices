bb058b7b3d067e15ac1029ede91395439fe32cb5
Mickey Donaghy <md401@srcf.ucam.org>
2024-10-02 20:47:18 +0900

    Wire in the writer, not working properly yet

--- src/main/scala/prices/Server.scala
+++ src/main/scala/prices/Server.scala
@@ -1,6 +1,7 @@
 package prices
 
 import cats.effect._
+import cats.effect.std.MapRef
 import cats.syntax.semigroupk._
 import com.comcast.ip4s._
 import fs2.Stream
@@ -8,8 +9,9 @@ import org.http4s.ember.client.EmberClientBuilder
 import org.http4s.ember.server.EmberServerBuilder
 import org.http4s.server.middleware.Logger
 import prices.config.Config
+import prices.data.{ InstanceDetails, InstanceKind }
 import prices.routes.{ ErrorHandling, InstanceKindRoutes, PricesRoutes }
-import prices.services.{ PricesService, SmartcloudService }
+import prices.services.{ CachePopulatorService, CachedDetailsService, PricesService, SmartcloudService }
 
 object Server {
 
@@ -23,7 +25,10 @@ object Server {
                               config.smartcloud.token
                             )
                           )
-      pricesService      = PricesService.make[IO](smartcloudService)
+      smartcloudCache <- MapRef.inConcurrentHashMap[ResourceIO, IO, InstanceKind, InstanceDetails]()
+      cachedDetails      = new CachedDetailsService(smartcloudCache)
+      cachePopulator     = new CachePopulatorService(smartcloudCache, smartcloudService, config.app)
+      pricesService      = PricesService.make[IO](cachedDetails)
       instanceKindRoutes = InstanceKindRoutes[IO](smartcloudService)
       pricesRoutes       = PricesRoutes[IO](pricesService)
       errorHandling      = new ErrorHandling[IO]
@@ -34,10 +39,11 @@ object Server {
              .withPort(Port.fromInt(config.app.port).get)
              .withHttpApp(Logger.httpApp(logHeaders = true, logBody = true)(httpApp))
              .build
-    } yield ()
+    } yield cachePopulator
 
-    Stream
-      .eval(resource.useForever)
+    Stream.resource(resource).flatMap { cachePopulator =>
+      Stream.never[IO] concurrently cachePopulator.cachePopulatorFiber
+    }
   }
 
 }
